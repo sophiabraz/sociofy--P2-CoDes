d75cbb4bd06a82e6a0f9fdda63d83f1131a23a88
sophiabraz <62796737+sophiabraz@users.noreply.github.com>
2020-11-30 14:51:08 -0300

    ..

--- projeto 2/js/tab.js
+++ projeto 2/js/tab.js
@@ -1,22 +1,29 @@
-function openPage(pageName, elmnt, color) {
-    // Hide all elements with class="tabcontent" by default */
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Remove the background color of all tablinks/buttons
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace("active", "");
-    }
-  
-    // Show the specific tab content
-    document.getElementById(pageName).style.display = "block";
-  
-    // Add the specific color to the button used to open the tab content
-    elmt.className("active")
-    elmnt.style.backgroundColor = color;
-    
+function openPage(pageName, elmnt, color, clicou) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+
+  // Remove the background color of all tablinks/buttons
+
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    if (tablinks[i].innerHTML == clicou ){
+      tablinks[i].classList.remove("unactive")
+      tablinks[i].classList.add("active")
+      
+    }
+    else{
+      tablinks[i].classList.remove("active")
+      tablinks[i].classList.add("unactive")
+    }
+  }
+
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "block";
+
+  // Add the specific color to the button used to open the tab content
+  
+}
